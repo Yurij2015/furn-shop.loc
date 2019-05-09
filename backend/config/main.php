1ded3d2437c91916750de6e70e36d6c5fc5965e1
Yurij2015 <nabor_2008@i.ua>
2019-05-09 23:04:56 +0300

    codecept

--- backend/config/main.php
+++ backend/config/main.php
@@ -37,14 +37,23 @@ return [
         'errorHandler' => [
             'errorAction' => 'site/error',
         ],
-        /*
+
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
             ],
         ],
-        */
+    ],
+    'as access' => [
+        'class' => 'yii\filters\AccessControl',
+        'except' => ['site/login', 'site/error'],
+        'rules' => [
+            [
+                'allow' => true,
+                'roles' => ['@'],
+            ],
+        ],
     ],
     'params' => $params,
 ];
